5bcbc5278180da92422a025c29a81716051faead
Mike Wagz <mike@selfaware.studio>
2024-11-21 16:26:28 -0500

    update link-env script for any number of destinations

--- scripts/link-env.js
+++ scripts/link-env.js
@@ -5,20 +5,22 @@ import { consola } from 'consola'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const envFile = resolve(__dirname, '../.env')
-const studioEnvFile = resolve(__dirname, '../studio/.env')
+const source = resolve(__dirname, '../.env')
+const destinations = [resolve(__dirname, '../studio/.env')]
 
-// Attempt to delete the file if it exists
 try {
-  await unlink(studioEnvFile)
+  for (const destination of destinations) {
+    await unlink(destination)
+  }
 } catch (error) {
-  // If the file does not exist, handle the error gracefully
   if (error.code !== 'ENOENT') {
-    consola.error('Error removing the .env file:', error)
-    process.exit(1) // Exit if the error is other than "File Not Found"
+    consola.error(`Error removing the .env file:`, error)
+    process.exit(1)
   }
 }
 
-// Create the symlink
-await symlink(envFile, studioEnvFile)
-consola.success('.env file symlink created successfully')
+for (const destination of destinations) {
+  await symlink(source, destination)
+}
+
+consola.success(`.env file symlink created successfully`)
