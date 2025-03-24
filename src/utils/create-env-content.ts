db1f35cef09dc6c4f65d2ad1c6aa064407b8d4b7
Mike Wagz <mike@selfaware.studio>
2025-03-23 23:35:25 -0400

    update env content

--- src/utils/create-env-content.ts
+++ src/utils/create-env-content.ts
@@ -5,12 +5,15 @@ export function createEnvContent(
 ) {
   const currentDate = new Date().toISOString().slice(0, 10)
   return (
+    `COMPATIBILITY_DATE=${currentDate}\n` +
     `SANITY_STUDIO_PROJECT_TITLE="${sanityProjectTitle}"\n` +
     `SANITY_STUDIO_PROJECT_ID=${sanityProjectId}\n` +
     `SANITY_STUDIO_DATASET=production\n` +
     `SANITY_STUDIO_API_VERSION=${currentDate}\n` +
     `SANITY_STUDIO_API_TOKEN=${sanityProjectToken}\n` +
     `SANITY_STUDIO_SLUG_INPUT_URL_PREFIX=\n` +
-    `SANITY_STUDIO_PROD_PREVIEW_URL=\n`
+    `SANITY_STUDIO_PROD_PREVIEW_URL=\n` +
+    `SITE_URL=\n` +
+    `GTAG_ID=\n`
   )
 }
